a061a49f5f544702ca18fe2ea014a17a78aa7a34
Joao Pedro <dp.joaopedro@gmail.com>
2025-02-02 17:54:30 -0300

    wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { BlotterComponent } from './blotter/blotter.component';
 import { ControlPanelComponent } from './control-panel/control-panel.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
   { path: 'blotter', component: BlotterComponent },
   { path: 'panel', component: ControlPanelComponent },
 ];
